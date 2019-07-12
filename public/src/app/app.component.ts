a86c0ccacff1df8eba95d30a5ba02cac18a5af2c
Jack Long <jack.richard.long@gmail.com>
2019-07-12 13:03:33 -0700

    Tasks

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {HttpService} from './http.service'
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'public';
+  tasks = [];
 	
 	constructor(private _httpService: HttpService){
-	
+  }
+  ngOnInit(){
+    this.getTaskFromService()
+  }
+  getTaskFromService(){
+    let observable = this._httpService.getTasks()
+    observable.subscribe(data =>{
+      console.log("Got tasks from service")
+      console.log(data)
+      this.tasks = data['data']
+    })
   }
 }
